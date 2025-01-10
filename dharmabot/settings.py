6a540f37d12336e4155574d2ea59178835c6a765
anjalichandran123 <anjalimeerachandran@gmail.com>
2025-01-10 13:31:32 +0530

    14 commit

--- dharmabot/settings.py
+++ dharmabot/settings.py
@@ -105,8 +105,7 @@ WSGI_APPLICATION = 'dharmabot.wsgi.application'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-ALLOWED_HOSTS = ['anjalichandran454.pythonanywhere.com']
-
+ALLOWED_HOSTS =['127.0.0.1', 'localhost', 'https://dharmaaiproject.onrender.com']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
