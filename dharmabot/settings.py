c7c80e95e8255da7a39710f1926fb4b658417900
anjalichandran123 <anjalimeerachandran@gmail.com>
2025-01-10 15:02:34 +0530

    18  commit

--- dharmabot/settings.py
+++ dharmabot/settings.py
@@ -106,6 +106,8 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 ALLOWED_HOSTS =['127.0.0.1', 'localhost', 'https://dharmaaiproject.onrender.com']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
